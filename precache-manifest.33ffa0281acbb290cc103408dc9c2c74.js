8f06adcc2afcdffac2dfb879d7eb047a8028ab4d
Faruk <Faruk@munideMacBook-Air.local>
2020-03-28 17:56:54 +0200

    Updates

---
+++ precache-manifest.33ffa0281acbb290cc103408dc9c2c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12938473a25965c0508a2269b35f488",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5ad5f4aa8ca8165a5e5b",
+    "url": "/robofriends/static/css/main.ae08ca2f.chunk.css"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/robofriends/static/js/2.8ca0bda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.8ca0bda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad5f4aa8ca8165a5e5b",
+    "url": "/robofriends/static/js/main.94cc83e0.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
